8022a60f8e896834a1db47e7d84d6d8577045cb4
Atharva <atharvamanjrekar28@yahoo.com>
2026-03-07 03:00:19 +0530

    Added the feature for cancellation of the slot and cancellation email is also been sent

--- backend/controllers/bookingControllers.js
+++ backend/controllers/bookingControllers.js
@@ -80,6 +80,7 @@ const createBooking = async (req, res) => {
 
     const booking = new Booking(req.body);
     await booking.save();
+    console.log(booking._id);
 
     // EMAIL SENDER
     const transporter = nodemailer.createTransport({
@@ -104,6 +105,8 @@ const createBooking = async (req, res) => {
     Service: ${req.body.service}
     Date: ${req.body.date}
     Time: ${req.body.time}
+    
+    Booking ID : ${booking._id}
 
     Please arrive 5-10 minutes before your scheduled time. If you need to reschedule or cancel your appointment, feel free to contact us.
 
@@ -111,7 +114,9 @@ const createBooking = async (req, res) => {
 
     Best regards,
     Studio 27 Salon
-`,
+    Mobile No: 9594257690
+    
+    `,
     });
 
     return res.status(201).json({
@@ -131,4 +136,60 @@ const createBooking = async (req, res) => {
   }
 };
 
-module.exports = { createBooking };
+const cancelBooking = async (req, res) => {
+  try {
+    const booking = await Booking.findByIdAndDelete(req.params.id);
+
+    if (!booking) {
+      return res.status(404).json({
+        message: "Booking not found",
+      });
+    }
+
+    // EMAIL SENDER
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+    });
+
+    await transporter.sendMail({
+      from: process.env.EMAIL_USER,
+      to: booking.email,
+      subject: `Studio 27 Booking Cancellation of Booking Id ${booking._id}`,
+      text: `
+Dear ${booking.name},
+
+Your appointment at Studio 27 has been successfully cancelled.
+
+Cancelled Booking Details:
+
+Service: ${booking.service}
+Date: ${booking.date}
+Time: ${booking.time}
+
+If this was done by mistake, you are welcome to book another slot through our website.
+
+Thank you for choosing Studio 27.
+
+Best regards,
+Studio 27 Salon
+Mobile No: 9594257690
+`,
+    });
+
+    res.status(200).json({
+      message: "Booking cancelled successfully and confirmation email sent",
+    });
+  } catch (error) {
+    console.log("ERROR:", error);
+
+    res.status(500).json({
+      message: "Server error",
+    });
+  }
+};
+
+module.exports = { createBooking, cancelBooking };
--- backend/routes/bookingRoutes.js
+++ backend/routes/bookingRoutes.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { createBooking } = require("../controllers/bookingControllers");
+const {
+  createBooking,
+  cancelBooking,
+} = require("../controllers/bookingControllers");
 // console.log(createBooking);
 router.post("/bookings", createBooking);
+router.delete("/bookings/:id", cancelBooking);
 
 module.exports = router;
